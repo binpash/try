095798695e0d6d94e2c4686afc7bddd3b1c531bc
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-12-07 03:59:26 -0500

    Modify proof of concept code to be more portable (workdirs in temp etc

--- overlay-sandbox/run-sandboxed.sh
+++ overlay-sandbox/run-sandboxed.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# Usage: ./run-sandboxed.sh <sandbox_label> <script_to_execute>
+# Usage: ./run-sandboxed.sh <script_to_execute>
 
 # TODO: Use $PASH_TOP to specify directory locations
 
-export pt="pash/temp/$1"
+export script_to_execute=${1?Script to execute not given}
+
+## Find the source code top directory
+export PASH_SPEC_TOP=${PASH_SPEC_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
+
+## Generate a temporary directory to store the workfiles
+mkdir -p /tmp/pash_spec
+export SANDBOX_DIR="$(mktemp -d /tmp/pash_spec/sandbox_XXXXXXX)/"
+
 export start_dir="$PWD"
 
-mkdir -p $pt/upperdir $pt/workdir $pt/temproot
-ls / | xargs -I '{}' mkdir $pt/temproot/'{}' $pt/workdir/'{}' $pt/upperdir/'{}'
+echo "Overlay directory: ${SANDBOX_DIR}"
+
+mkdir -p "${SANDBOX_DIR}/upperdir" "${SANDBOX_DIR}/workdir" "${SANDBOX_DIR}/temproot"
+ls / | xargs -I '{}' mkdir "${SANDBOX_DIR}"/temproot/'{}' "${SANDBOX_DIR}"/workdir/'{}' "${SANDBOX_DIR}"/upperdir/'{}'
 
 # Me overlay-mount each root directory separately 
 # (instead of all at once) because some directories cannot be overlayed. 
@@ -22,9 +32,9 @@ ls / | xargs -I '{}' mkdir $pt/temproot/'{}' $pt/workdir/'{}' $pt/upperdir/'{}'
 #                  the newly created user namespace.
 # --user: The process will have a distinct set of UIDs, GIDs and
 #         capabilities.
-unshare --mount --map-root-user --user ./mount-and-execute.sh $2
+unshare --mount --map-root-user --user "${PASH_SPEC_TOP}/overlay-sandbox/mount-and-execute.sh" "${SANDBOX_DIR}" "${script_to_execute}"
 
-changed_files=`find $PWD/$pt/upperdir/* -type f`
+changed_files=`find ${SANDBOX_DIR}/upperdir/* -type f`
 
 if [ !  -z  "$changed_files"  ]; then
     echo "Changes detected in the following files:"
