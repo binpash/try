2a4869216ccbcbc34149c12ce1e46e757f967622
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-04-24 14:36:56 -0400

    Add structure to handle variables

--- overlay-sandbox/chroot_executable.sh
+++ overlay-sandbox/chroot_executable.sh
@@ -1,6 +1,3 @@
 mount -t proc proc /proc &&
 cd $start_dir && 
 source ${script_to_execute}
-## Save the output shell variables to a file (to pass to the outside context)
-source "$RUNTIME_DIR/pash_declare_vars.sh" "$OUTPUT_VARIABLE_FILE"
-pash_redir_output echo "Sandbox ${CMD_ID} Output variables saved in: $OUTPUT_VARIABLE_FILE"
