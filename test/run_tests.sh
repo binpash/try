c257cc4f14577c9cdce80c50352111e65bea3318
a1346054 <36859588+a1346054@users.noreply.github.com>
2023-07-06 11:25:16 +0000

    test/run_tests.sh: Test command directly

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -36,8 +36,7 @@ cleanup()
 
 test_read_from_run_dir()
 {
-    ls /run/systemd > /dev/null
-    if [[ $? -ne 0 ]]; then
+    if ls /run/systemd >/dev/null 2>&1; then
         echo "Cannot read from /run/systemd."
         return 1
     fi
