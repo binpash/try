1c567c5f7c556236e239065a8e061e7ce2ffd9dd
Eric Zhu <eric@ericz.me>
2023-10-31 00:58:04 -0400

    fix(tests/net): fix style for net tests

--- test/network.sh
+++ test/network.sh
@@ -10,8 +10,9 @@ TRY="$TRY_TOP/try"
 # Test if curl fails when network is unshared
 # curl exit code 7 means Failed to connect to host.
 "$TRY" -x curl 1.1
-if [ $? -eq 7 ]; then
-    return 0;
+if [ $? -eq 7 ]
+then
+    return 0
 else
-    return 1;
+    return 1
 fi
