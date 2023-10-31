b03f85469d09a5957c608df11c8756b951a9f322
Eric Zhu <eric@ericz.me>
2023-10-31 00:55:00 -0400

    fix(tests/net): fix docs for net tests

--- test/network.sh
+++ test/network.sh
@@ -4,9 +4,10 @@ TRY_TOP="${TRY_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-
 TRY="$TRY_TOP/try"
 
 # Test if network works normally
+# using curl due to #131 (1.1 expands to 1.0.0.1)
 "$TRY" curl 1.1 || return 1
 
-# Test if ping fails when network is unshared
+# Test if curl fails when network is unshared
 # curl exit code 7 means Failed to connect to host.
 "$TRY" -x curl 1.1
 if [ $? -eq 7 ]; then
