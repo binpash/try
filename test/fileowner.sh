8bcf1e1fd4e5c22567b08f1c49a122efa5cbf548
Ezri Zhu <me@ezrizhu.com>
2024-02-17 10:15:57 +0000

    test: fileowner test, requires root

---
+++ test/fileowner.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+TRY_TOP="${TRY_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}"
+TRY="$TRY_TOP/try"
+
+cleanup() {
+    cd /
+
+    if [ -d "$try_workspace" ]
+    then
+        rm -rf "$try_workspace" >/dev/null 2>&1
+    fi
+
+    if [ -f "$expected" ]
+    then
+        rm "$expected"
+    fi
+
+    if [ -f "$target" ]
+    then
+        rm "$target"
+    fi
+}
+
+trap 'cleanup' EXIT
+
+try_workspace="$(mktemp -d)"
+cd "$try_workspace" || return 9
+touch test
+
+# Set up expected output
+expected="$(mktemp)"
+ls -l >"$expected"
+
+# Set up target output
+target="$(mktemp)"
+
+sudo "$TRY" ls -l > "$target" || return 1
+diff -q "$expected" "$target"
