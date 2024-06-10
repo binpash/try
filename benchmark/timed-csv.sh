862e36b0a712e9de828fcab1127bf1601140240e
Ezri Zhu <me@ezrizhu.com>
2024-06-09 23:11:16 -0400

    ms

--- benchmark/timed-csv.sh
+++ benchmark/timed-csv.sh
@@ -18,7 +18,8 @@ while IFS= read -r line; do
 
     # Calculate the delta t if it's not the first line
     if [[ $prev_timestamp != 0 ]]; then
-        delta_t=$(echo "$timestamp - $prev_timestamp" | bc)
+        # milisecond
+        delta_t=$(echo "($timestamp - $prev_timestamp) * 1000" | bc)
         printf "%.9f;" "$delta_t"
     fi
 
