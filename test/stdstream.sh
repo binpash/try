441b21b234f53ddc27893794e9905d8f26cd1b20
Ezri Zhu <me@ezrizhu.com>
2024-07-10 18:38:50 -0400

    stdstream support

---
+++ test/stdstream.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TRY_TOP="${TRY_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree 2>/dev/null || echo "${0%/*}")}"
+TRY="$TRY_TOP/try"
+
+cmdfile="$(mktemp)"
+
+cat > "$cmdfile" <<"EOF"
+file /dev/stdin
+file /dev/stdout
+file /dev/stderr
+EOF
+
+chmod +x "$cmdfile"
+
+result=$("$TRY" "$cmdfile")
+expected=$(sh "$cmdfile")
+
+[ "$result" = "$expected" ] || exit 1
