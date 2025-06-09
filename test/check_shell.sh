91449064767aec1faaac4f17fbc600f85aaf917a
Ioa1 <ioaunknown2@gmail.com>
2025-06-09 13:55:33 -0400

    Added setting try shell and check_shell test. Fixes (#7)

---
+++ test/check_shell.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+TRY_TOP="${TRY_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree 2>/dev/null || echo "${0%/*}")}"
+TRY="$TRY_TOP/try"
+
+cleanup() {
+  cd /
+
+  if [ -d "$try_workspace" ]; then
+    rm -rf "$try_workspace" >/dev/null 2>&1
+  fi
+
+  if [ -f "$expected" ]; then
+    rm "$expected"
+  fi
+
+  if [ -f "$out" ]; then
+    rm "$out"
+  fi
+
+}
+
+check_case() {
+  try_shell="$1"
+  shell="$2"
+  expected_output="$3"
+  case="$4"
+
+  expected="$(mktemp)"
+  out="$(mktemp)"
+  echo "$expected_output" >"$expected"
+  TRY_SHELL="$try_shell" SHELL="$shell" "$TRY" 'echo "$TRY_SHELL"' >"$out" || exit 1
+
+  if ! diff -q $expected $out; then
+    exit "$case"
+  fi
+
+  rm "$expected"
+  rm "$out"
+
+}
+
+trap 'cleanup' EXIT
+
+check_case "/bin/bash" "/bin/sh" "/bin/bash" "1"
+
+check_case "" "/bin/bash" "/bin/bash" "2"
+
+if [ "$CI" = "true" ]; then
+  saved_shell=$(grep -e "^$LOGNAME" /etc/passwd | cut -d: -f7)
+  sudo apt install -y zsh
+  sudo chsh "$LOGNAME" --shell=/usr/bin/zsh
+  check_case "" "" "/usr/bin/zsh" "3"
+  #just in case the user calls this regerate old shell
+  sudo chsh "$LOGNAME" --shell="$saved_shell"
+fi
+
+check_case "" "" "/bin/sh" "4"
