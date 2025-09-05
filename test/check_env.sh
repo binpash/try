ef5bfb7d114b36f0c9376b752bdde6c634e31b7b
Ioa1 <ioaunknown2@gmail.com>
2025-09-05 17:42:47 -0400

    Synced try environments (#153)

---
+++ test/check_env.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+TRY_TOP="${TRY_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree 2>/dev/null || echo "${0%/*}")}"
+TRY="$TRY_TOP/try"
+
+cleanup() {
+
+  if [ -f "$expected" ]; then
+    rm "$expected"
+  fi
+
+  if [ -f "$out" ]; then
+    rm "$out"
+  fi
+
+}
+
+out="$(mktemp)"
+expected="$(mktemp)"
+
+echo "$OLDPWD" >"$expected"
+
+"$TRY" 'echo "$TEMP_OLDPWD"' >"$out" || exit 1
+
+if ! diff -q "$expected" "$out"; then
+  exit 2
+fi
+
+rm "$out"
+rm "$expected"
+
+trap 'cleanup' EXIT
