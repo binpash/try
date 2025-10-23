29127bbf609498d806a69a06412c9779171ab127
Michael Greenberg <michael@greenberg.science>
2025-10-23 17:22:27 -0400

    add -q flag for quieter output from try-summary (not something try will produce); aligns try-summary and the built-in shell fallback output

--- test/summary.sh
+++ test/summary.sh
@@ -32,12 +32,13 @@ echo 'fail' >file_2.txt
 touch target
 
 try_example_dir=$(mktemp -d)
-"$TRY" -D "$try_example_dir" "touch file_1.txt; echo test >file_2.txt; rm file.txt.gz; rm target; mkdir target; mkdir new_dir" || exit 1
+"$TRY" -D "$try_example_dir" "touch file_1.txt; echo test >file_2.txt; rm file.txt.gz; rm target; mkdir target; mkdir new_dir new_dir/nested" || exit 1
 "$TRY" summary "$try_example_dir" >summary.out || exit 2
 
 # Check that the summary correctly identifies every change
-grep -qx -e "$PWD/file_1.txt (added)"         summary.out || exit 3
-grep -qx -e "$PWD/file_2.txt (modified)"      summary.out || exit 4
-grep -qx -e "$PWD/file.txt.gz (deleted)"      summary.out || exit 5
-grep -qx -e "$PWD/target (replaced with dir)" summary.out || exit 6
-grep -qx -e "$PWD/new_dir (created dir)"      summary.out || exit 7
+grep -qx -e "$PWD/file_1.txt (added)"           summary.out || exit 3
+grep -qx -e "$PWD/file_2.txt (modified)"        summary.out || exit 4
+grep -qx -e "$PWD/file.txt.gz (deleted)"        summary.out || exit 5
+grep -qx -e "$PWD/target (replaced with dir)"   summary.out || exit 6
+grep -qx -e "$PWD/new_dir (created dir)"        summary.out || exit 7
+grep -qx -e "$PWD/new_dir/nested (created dir)" summary.out || exit 8
