e676e838c4bcd9b2419f09895b46dede05c4302b
Ioannis Magkaniotis <142560008+Ioa1@users.noreply.github.com>
2025-11-07 19:12:43 +0000

    ensure environment in try is similar to host (#153) (#207)
    Co-authored-by: Michael Greenberg <michael@greenberg.science>

---
+++ test/check_env.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+TRY_TOP="${TRY_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree 2>/dev/null || echo "${0%/*}")}"
+TRY="$TRY_TOP/try"
+
+cleanup() {
+
+  if [ -f "$expected" ]; then
+    rm "$expected"
+  fi
+
+  if [ -f "$out" ]; then
+    rm "$out"
+  fi
+
+}
+
+out="$(mktemp)"
+expected="$(mktemp)"
+
+echo "$PWD $OLDPWD" >"$expected"
+
+# shellcheck disable=SC2016
+"$TRY" 'echo "$PWD $OLDPWD"' >"$out" || exit 1
+
+if ! diff -q "$expected" "$out"; then
+  exit 2
+fi
+
+rm "$out"
+rm "$expected"
+
+trap 'cleanup' EXIT
