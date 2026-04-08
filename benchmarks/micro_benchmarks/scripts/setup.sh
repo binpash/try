04ac5ac119bf14335b4c4f25e9af0e49a7e70439
Evangelos Lamprou <vagos@lamprou.xyz>
2026-04-08 15:36:51 -0400

    Fix path
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- benchmarks/micro_benchmarks/scripts/setup.sh
+++ benchmarks/micro_benchmarks/scripts/setup.sh
@@ -9,7 +9,7 @@ trap 'rm -rf "$build_dir"' EXIT
 sudo install -m 0755 "${repo_root}/try" /usr/local/bin/try
 sudo install -m 0755 "${repo_root}/scripts/try-timed" /usr/local/bin/try-timed
 sudo install -m 0755 "${repo_root}/scripts/try-patched" /usr/local/bin/try-patched
-sudo install -m 0755 "${repo_root}/try-ae/try/trace_v2.py" /usr/local/bin/trace_v2.py
+sudo install -m 0755 "${repo_root}/utils/parse_trace.py" /usr/local/bin/trace_v2.py
 
 gcc -g -Wall -O2 -I"${repo_root}/utils" -c "${repo_root}/utils/ignores.c" -o "${build_dir}/ignores.o"
 gcc -g -Wall -O2 -I"${repo_root}/utils" -c "${repo_root}/utils/try-summary.c" -o "${build_dir}/try-summary.o"
@@ -20,7 +20,7 @@ gcc -g -Wall -O2 -o "${build_dir}/try-commit" "${build_dir}/ignores.o" "${build_
 sudo install -m 0755 "${build_dir}/try-summary" /usr/local/bin/try-summary
 sudo install -m 0755 "${build_dir}/try-commit" /usr/local/bin/try-commit
 
-cd "${repo_root}/try-ae/try/gidmapper"
+cd "${repo_root}/utils/gidmapper"
 cargo build --release
 sudo install -m 0755 target/release/gidmapper /usr/local/bin/gidmapper
 sudo setcap 'CAP_SETGID=ep' /usr/local/bin/gidmapper
