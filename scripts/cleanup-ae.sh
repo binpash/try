2910ff7d20de638f9df52550f3e3022e13f2cf36
Evangelos Lamprou <vagos@lamprou.xyz>
2026-04-08 14:52:26 -0400

    Fixup cleanup script
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- scripts/cleanup-ae.sh
+++ scripts/cleanup-ae.sh
@@ -8,8 +8,6 @@ cd "$ROOT_DIR"
 vagrant halt debian
 
 rm -rf benchmarks/results
-rm -rf benchmarks/dependency_tracking/results/incr
-rm -rf benchmarks/partial_specification_mining/caruca/results/caruca
 rm -rf artifacts/paper_eval
 
 vagrant destroy -f debian
